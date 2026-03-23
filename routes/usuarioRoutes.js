9c333a97e8e7d87728d3bcb669080617a4233078
Jael240784 <240784@utxicotepec.edu.mx>
2026-03-23 14:13:38 -0600

    Bienes raices - seccion 07
    add: Se realizó el formulario y validaciones para la restauración de contraseñas, aun pendiente la actulización de las mismas en la bd

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,20 +1,89 @@
 import express from "express";
+import { body } from "express-validator";
+
 import {
   formularioLogin,
   formularioRegistro,
   formularioRecuperacion,
   registrarUsuario,
+  autenticarUsuario,
+  solicitarRecuperacionPassword,
   paginaConfirmacion,
+  formularioActualizacionPassword,
+  resetearPassword,
 } from "../controllers/usuarioControllers.js";
 
 const router = express.Router();
 
+// GET
 router.get("/login", formularioLogin);
 router.get("/registro", formularioRegistro);
 router.get("/recuperarPassword", formularioRecuperacion);
+router.get("/confirma/:token", paginaConfirmacion);
+router.get("/actualizarPassword/:token", formularioActualizacionPassword);
 
-router.get("/confirmar/:token", paginaConfirmacion);
+// POST REGISTRO
+router.post(
+  "/registro",
+  body("nombreUsuario")
+    .trim()
+    .notEmpty()
+    .withMessage("El nombre de la persona no puede ser vacío"),
 
-router.post("/registro", registrarUsuario);
+  body("emailUsuario")
+    .trim()
+    .notEmpty()
+    .withMessage("El correo electrónico no puede ser vacío")
+    .isEmail()
+    .withMessage("El correo electrónico no tiene un formato adecuado"),
+
+  body("passwordUsuario")
+    .trim()
+    .notEmpty()
+    .withMessage("La contraseña parece estar vacía")
+    .isLength({ min: 8, max: 30 })
+    .withMessage("La longitud de la contraseña debe ser entre 8 y 30 caractéres"),
+
+  body("confirmarPasswordUsuario")
+    .trim()
+    .notEmpty()
+    .withMessage("La confirmación de contraseña es obligatoria")
+    .custom((value, { req }) => value === req.body.passwordUsuario)
+    .withMessage("Ambas contraseñas deben ser iguales"),
+
+  registrarUsuario
+);
+
+// POST LOGIN
+router.post(
+  "/login",
+  body("emailUsuario")
+    .trim()
+    .notEmpty()
+    .withMessage("El correo electrónico no puede ser vacío")
+    .isEmail()
+    .withMessage("El correo electrónico no tiene un formato adecuado"),
+
+  body("passwordUsuario")
+    .trim()
+    .notEmpty()
+    .withMessage("La contraseña es obligatoria"),
+
+  autenticarUsuario
+);
+
+// POST RECUPERAR
+router.post(
+  "/recuperar",
+  body("emailUsuario")
+    .trim()
+    .notEmpty()
+    .withMessage("El correo electrónico no puede ser vacío")
+    .isEmail()
+    .withMessage("El correo electrónico no tiene un formato adecuado"),
+  solicitarRecuperacionPassword
+);
+
+router.post("/recuperarPassword", resetearPassword);
 
 export default router;
